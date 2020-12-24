04a28dfdd8736b8a12928928e141c41c1d15e0da
iamarcadie <dorosenco12@gmail.com>
2020-12-24 17:23:38 +0200

    cam stream

--- app.py
+++ app.py
@@ -1,6 +1,47 @@
+import io
+import socket
+import struct
+from PIL import Image
+import matplotlib.pyplot as pl
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def stream():
+    server_socket = socket.socket()
+    server_socket.bind(('178.138.195.170', 8000))  # ADD IP HERE
+    server_socket.listen(0)
+
+    # Accept a single connection and make a file-like object out of it
+    connection = server_socket.accept()[0].makefile('rb')
+    try:
+        img = None
+        while True:
+            # Read the length of the image as a 32-bit unsigned int. If the
+            # length is zero, quit the loop
+            image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
+            if not image_len:
+                break
+            # Construct a stream to hold the image data and read the image
+            # data from the connection
+            image_stream = io.BytesIO()
+            image_stream.write(connection.read(image_len))
+            # Rewind the stream, open it as an image with PIL and do some
+            # processing on it
+            image_stream.seek(0)
+            image = Image.open(image_stream)
+            
+            if img is None:
+                img = pl.imshow(image)
+            else:
+                img.set_data(image)
+
+            pl.pause(0.01)
+            pl.draw()
+
+            print('Image is %dx%d' % image.size)
+            image.verify()
+            print('Image is verified')
+    finally:
+        connection.close()
+        server_socket.close()
